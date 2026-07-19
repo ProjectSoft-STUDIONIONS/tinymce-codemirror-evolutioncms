a2fa59e3ba2cc3f6025247c5716e616ac3125b0f
ProjectSoft <projectsoft2009@yandex.ru>
2026-07-19 16:34:15 +0400

    update

---
+++ plugins/codemirror/langs/fr-FR.js
@@ -0,0 +1,1 @@
+tinymce.addI18n("fr-FR",{"The source HTML code of the CodeMirror plugin":"The source HTML code\nof the CodeMirror plugin","Start search":"Rechercher","Find next":"Chercher suiv.","Find previous":"Chercher pr\xe9c.",Replace:"Replace","Replace all":"Replace all"});
